1d84ee5a507b03d1b5e09151559d6bed6039ed70
taylor <tailor@u.washington.edu>
2014-02-02 12:32:42 -0800

    Git install: git reset --hard v1.8.5.3 since gerrit repo cant read revision v1.9-rc1
    Change-Id: If6abd71b8c1d2a8369ba878af07516f9570d0542

--- compile_lastest_git.sh
+++ compile_lastest_git.sh
@@ -2,4 +2,11 @@
 
 mkdir -p /usr/local/src
 git clone https://github.com/gitster/git /usr/local/src/git
+
+# today's git version is v1.9-rc1 and gerrit repo barfs when it tries to
+# read v1.9-rc1 as a revision number, but repo reads v1.8.5.3 no
+# problem, so refert to a git version that repo knows how to read
+cd /usr/local/src/git
+git reset --hard v1.8.5.3
+
 make -C /usr/local/src/git INSTALL=/usr/bin/install install
