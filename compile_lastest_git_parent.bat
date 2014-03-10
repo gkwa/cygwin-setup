843d397fb748ce53277e9d5dbd752c7d10331d2c
taylor <tailor@u.washington.edu>
2014-03-09 18:01:17 -0700

    compile_lastest_git_parent.bat now calls correct compile_lastest_git_child.sh script
    Change-Id: I720c5c47708593a90f4759df4581a247c4d0f4c6

--- compile_lastest_git_parent.bat
+++ compile_lastest_git_parent.bat
@@ -4,6 +4,6 @@ set PATH=C:\cygwin64\bin;%PATH%
 rem Git v1.7.9 doesn't recognize push.default=simple:
 git config --global --unset push.default simple
 
-sh -x compile_lastest_git.sh
+sh -x compile_lastest_git_child.sh
 rem turn push.simple back on
 git config --global push.default simple
