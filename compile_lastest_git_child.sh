981dfe196f343770107a38abbaab562c07099de2
taylor <tailor@u.washington.edu>
2014-04-06 14:38:26 -0700

    If we're running with /usr/local/src/git dir already present, then pull updates
    Change-Id: I80cd4fb4084ac43e0d92add5948c9ce915a04136

--- compile_lastest_git_child.sh
+++ compile_lastest_git_child.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 mkdir -p /usr/local/src
-git clone https://github.com/gitster/git /usr/local/src/git
+if test ! -d /usr/local/src/git
+then
+    git clone https://github.com/gitster/git /usr/local/src/git
+else
+    pushd /usr/local/src/git
+    git pull
+    popd
+fi
 
 # today's git version is v1.9-rc1 and gerrit repo barfs when it tries to
 # read v1.9-rc1 as a revision number, but repo reads v1.8.5.3 no
