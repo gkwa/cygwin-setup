dfedaa122f252a9a5abc2b6b0df18532bcaaeb4b
taylor <tailor@u.washington.edu>
2014-04-13 14:38:32 -0700

    Upgrade git to v1.9.2
    Change-Id: I1adb38a50901cd94836149a2db0435b9d936208a

--- compile_lastest_git_child.sh
+++ compile_lastest_git_child.sh
@@ -14,6 +14,6 @@ fi
 # read v1.9-rc1 as a revision number, but repo reads v1.8.5.3 no
 # problem, so refert to a git version that repo knows how to read
 cd /usr/local/src/git
-git reset --hard v1.9.1
+git reset --hard v1.9.2
 
 make -C /usr/local/src/git INSTALL=/usr/bin/install install
