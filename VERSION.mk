85097185d52662b8e694231fdf7ea3085b575c43
taylormonacelli <tailor@u.washington.edu>
2014-09-04 22:37:22 -0700

    When installing dotfiles to $HOME, set master to track my upstream origin/master

--- VERSION.mk
+++ VERSION.mk
@@ -1,6 +1,6 @@
 majorv=0
 minorv=0
-patchv=76
+patchv=77
 qualifierv=0
 
 version=$(majorv).$(minorv).$(patchv).$(qualifierv)
--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -7,3 +7,4 @@ git init
 git remote add origin https://github.com/taylormonacelli/dotfiles.git
 git fetch --all
 git reset --hard origin/master
+git branch --set-upstream-to origin/master
