7956c4e969495f0c770e129c502b68a20cb08cf2
taylormonacelli <taylormonacelli@gmail.com>
2014-12-31 20:19:45 -0800

    Fix sshd on Windows server 2008

--- sshd-auto-setup-controller.cmd
+++ sshd-auto-setup-controller.cmd
@@ -8,18 +8,8 @@ rem http://typesafe.be/2011/05/28/automating-openssh-installations-on-windows-se
 set PATH=C:\cygwin\bin;%PATH%
 set PATH=C:\cygwin64\bin;%PATH%
 
-net stop sshd 2>NUL
-
-bash -c "/usr/bin/openssl rand 35 -base64 | tr -d ' ' | tr -d '\n' | tr -d '\r' >/tmp/out.pass"
-bash -c "/bin/ssh-host-config -y -c ntsec -u sshd_account -w $(cat /tmp/out.pass)"
-bash -c "/bin/sed -i.bak -e 's/#PrintMotd .*/PrintMotd no/' /etc/sshd_config"
-bash -c "/bin/sed -i.bak -e 's/#Port .*/Port 6045/' /etc/sshd_config"
-
-rm -f /tmp/out.pass
-
+sh -x sshd-auto-setup.sh
 
 rem new style firewall / add exception
 netsh advfirewall firewall add rule name=SSH dir=in action=allow protocol=tcp localport=22 2>NUL
 netsh advfirewall firewall add rule name="SSH obfuscated on tcp/6045" dir=in action=allow protocol=tcp localport=6045 2>NUL
-
-net start sshd
