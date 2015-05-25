9350a172d2308ca23d403c741098173bad11c961
Taylor Monacelli <taylormonacelli@gmail.com>
2015-05-25 14:56:50 -0700

    yada-yada

--- emacsserver.sh
+++ emacsserver.sh
@@ -2,6 +2,5 @@
 
 if test 0 -le $(find /tmp/emacs* -iname server | wc -l)
 then
-    mintty --hold never --window min --icon /bin/emacs.ico \
-	   --exec bash --login -c 'emacs &'
+    mintty --window min --icon /bin/emacs.ico --exec bash --login -c 'emacs &'
 fi
