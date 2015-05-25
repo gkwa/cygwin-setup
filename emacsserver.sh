e443576f5fb2517cddc9e5169aee31b22a6d6c34
Taylor Monacelli <taylormonacelli@gmail.com>
2015-05-25 14:58:33 -0700

    yada-yada

--- emacsserver.sh
+++ emacsserver.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if test 0 -le $(find /tmp/emacs* -iname server | wc -l)
+if test 1 -le $(find /tmp/emacs* -iname server | wc -l)
 then
     mintty --window min --icon /bin/emacs.ico --exec bash --login -c emacs &
 fi
