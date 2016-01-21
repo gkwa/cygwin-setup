5bf16208320418eb95a48c5acec60ab9f76b7813
Taylor Monacelli <taylor.monacelli@streambox.com>
2016-01-21 12:37:15 -0800

    Speed up git pull on dotfiles and bup by just fetching depth=1
    After git pull/fetch --depeth=1, then later you can merge in old stuff
    with git pull.

--- dotfiles-install.sh
+++ dotfiles-install.sh
@@ -5,6 +5,6 @@ PATH=/usr/local/bin:$PATH
 
 git init
 git remote add origin https://github.com/taylormonacelli/dotfiles.git
-git fetch --all
+git fetch --depth 1
 git reset --hard origin/master
 git branch --set-upstream-to origin/master
