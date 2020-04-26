45e6671cc528b90db83f4988d9c7d542e80264c8
Taylor Monacelli <taylormonacelli@gmail.com>
2020-04-26 08:02:11 -0700

    chore: cleanup: remove mosh from ssh, i've not used it

--- sshd-auto-setup-controller.cmd
+++ sshd-auto-setup-controller.cmd
@@ -11,6 +11,3 @@ sh -x sshd-auto-setup.sh
 rem new style firewall / add exception
 netsh advfirewall firewall add rule name=SSH dir=in action=allow protocol=tcp localport=22 2>NUL
 netsh advfirewall firewall add rule name="SSH obfuscated on tcp/6045" dir=in action=allow protocol=tcp localport=6045 2>NUL
-
-# Open firewall for Mosh Mobile Shell
-netsh advfirewall firewall add rule name="Mosh Mobile Shell" protocol=UDP dir=in localport=60000-61000 action=allow
